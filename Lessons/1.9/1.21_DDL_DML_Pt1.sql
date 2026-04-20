ebf1fca73992ed1d2bd1ca2031e31184ec7c1a3e
Luke Barousse <i'm not sharing this on screen. lol>
2026-04-20 18:00:33 +0900

    gogo

--- Lessons/1.9/1.21_DDL_DML_Pt1.sql
+++ Lessons/1.9/1.21_DDL_DML_Pt1.sql
@@ -1,10 +1,30 @@
+--.read Lessons/1.9/1.21_DDL_DML_Pt1.sql
+
+USE data_jobs;
+
+DROP DATABASE IF EXISTS jobs_mart;
+
 CREATE DATABASE IF NOT EXISTS jobs_mart;
 
 SHOW DATABASES;
 
--- DROP DATABASE IF EXISTS jobs_mart;
-
 SELECT *
 FROM information_schema.schemata;
 
-CREATE SCHEMA jobs_mart.staging;
+use jobs_mart;
+
+DROP SCHEMA if exists staging;
+CREATE SCHEMA if not exists staging;
+
+
+
+CREATE TABLE if not exists staging.preferred_roles (
+    role_id INTEGER,
+    role_name VARCHAR
+);
+
+SELECT *
+FROM information_schema.tables
+WHERE table_catalog = 'jobs_mart';
+
+drop table if EXISTS main.preferred_roles;
